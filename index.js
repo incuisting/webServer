b87e192acd4bc452070e4ddcf3eb35fb7b2bc666
xuyanting <393146040@qq.com>
2018-12-24 17:12:05 +0800

    通过 writeHead 来添加响应头，第一个参数可以添加状态码

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { createServer } = require('http');
 
 createServer((request, response) => {
-  response.setHeader('content-type', 'application/json');
+  response.writeHead(200, {
+    'content-type': 'application/json',
+  });
   response.end(JSON.stringify({ a: 1 }));
 }).listen(8080);

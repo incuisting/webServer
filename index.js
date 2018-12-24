ac5d25e70d47dac9ae929736462373abee1a9992
xuyanting <393146040@qq.com>
2018-12-24 16:17:08 +0800

    通过调用response 上setheader的方式设置请求头

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { createServer } = require('http');
 
 createServer((request, response) => {
-  const lan = request.headers['accept-language'];
-  response.end(lan);
+  response.setHeader('content-type', 'application/json');
+  response.end(JSON.stringify({ a: 1 }));
 }).listen(8080);

174bf59a03f082f82efefb6df06d36701b772ae6
xuyanting <393146040@qq.com>
2018-12-27 11:31:41 +0800

    body content

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const { createServer } = require('http');
 const url = require('url');
 
 createServer((req, res) => {
-  const { query } = url.parse(req.url, true);
-  if (query.name) {
-    res.end(query.name);
+  if (req.method === 'POST') {
+    let data = '';
+    req.on('data', chunk => {
+      data += chunk;
+    });
+    req.on('end', () => {
+      try {
+        const requestData = JSON.parse(data);
+        requestData.ourMessage = 'success';
+        res.writeHead(200, {
+          'Content-Type': 'application/json',
+        });
+        res.end(JSON.stringify(requestData));
+      } catch (e) {
+        res.setStatus = 400;
+        res.end('错误json');
+      }
+    });
   } else {
-    res.end('no name');
+    res.statusCode = 400;
+    res.end('Unsupported method, please POST a JSON object');
   }
 }).listen(8080);
